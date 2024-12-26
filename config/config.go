2e93fe6ec3b266afaf0cc2a16d2af2dbdfdd2538
vsem <virtualik@gmail.com>
2024-12-26 20:15:31 +0100

    Initial commit

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    APIKey       string
+    Port         string
+    DBConnString string
+}
+
+func LoadConfig() Config {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Ошибка загрузки .env файла: %v", err)
+    }
+
+    return Config{
+        APIKey:       os.Getenv("API_KEY"),
+        Port:         os.Getenv("PORT"),
+        DBConnString: os.Getenv("DB_CONN_STRING"),
+    }
+}
